a4034ca1c79494cd9c7712b05a8fc35fe5993a91
Fallen_Breath <nellaforax@outlook.com>
2024-01-07 17:46:49 +0800

    tidy up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
+import {Metadata, Viewport} from "next";
+import {siteConfig} from "@/config/site";
+import {fontSans} from "@/config/fonts";
+import {Providers} from "./providers";
+import {Navbar} from "@/components/navbar";
+import {Link} from "@nextui-org/link";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
@@ -13,16 +13,19 @@ export const metadata: Metadata = {
 		template: `%s - ${siteConfig.name}`,
 	},
 	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
 	icons: {
 		icon: "/favicon.ico",
 		shortcut: "/favicon-16x16.png",
 		apple: "/apple-touch-icon.png",
 	},
 };
+
+export const viewport: Viewport = {
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+}
 
 export default function RootLayout({
 	children,
@@ -31,14 +34,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<head />
 			<body
 				className={clsx(
 					"min-h-screen bg-background font-sans antialiased",
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {Link} from "@nextui-org/link";
+import {Snippet} from "@nextui-org/snippet";
+import {Code} from "@nextui-org/code"
+import {button as buttonStyles} from "@nextui-org/theme";
+import {siteConfig} from "@/config/site";
+import {subtitle, title} from "@/components/primitives";
+import {GithubIcon} from "@/components/icons";
 
 export default function Home() {
 	return (
