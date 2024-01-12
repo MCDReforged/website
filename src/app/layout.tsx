0c462bbfe3e3b6ff3e91c09f3d4e37d478e59b27
Fallen_Breath <nellaforax@outlook.com>
2024-01-13 00:40:54 +0800

    init and mantine setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '@mantine/core/styles.css'
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +16,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  // noinspection HtmlRequiredTitleElement
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript/>
+      </head>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
