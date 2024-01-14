0f4a5c8ed971082d0f283a6e99f7a77a44856eef
Fallen_Breath <nellaforax@outlook.com>
2024-01-15 02:26:17 +0800

    favicon thing

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   title: 'MCDReforged',
   titleTemplate: '%s - MCDReforged',
-  favicon: "/mcdr.svg",
+  favicon: "/favicon.svg",
   links: {
     docs: "https://docs.mcdreforged.com",
     github: "https://github.com/Fallen-Breath/MCDReforged",
