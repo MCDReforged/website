b32d668d63711d9303b8336a896aa9068e2baaa2
Fallen_Breath <nellaforax@outlook.com>
2024-01-15 17:30:02 +0800

    mantine tree shaking

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withNextIntl = require('next-intl/plugin')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withNextIntl({
+  experimental: {
+    // https://mantine.dev/guides/next/#app-router-tree-shaking
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 })
 
 module.exports = nextConfig
