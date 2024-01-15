c7148fa14ee25c26f61f9a9dae95f5b74faecf2c
Fallen_Breath <nellaforax@outlook.com>
2024-01-16 02:34:15 +0800

    temp react-hydration-error fix

--- src/components/ui/theme-switch.tsx
+++ src/components/ui/theme-switch.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import { ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export function ThemeSwitch({...props}) {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme()
-  const icon = colorScheme === 'light' ? <IconSun stroke={1.5} /> : <IconMoon stroke={1.5} />
+
+  // https://nextjs.org/docs/messages/react-hydration-error#solution-1-using-useeffect-to-run-on-the-client-only
+  const [isServer, setIsServer] = useState(true)
+  useEffect(() => {
+    setIsServer(false)
+  }, [])
+
+  const icon = isServer || colorScheme === 'light' ? <IconSun stroke={1.5} /> : <IconMoon stroke={1.5} />
   return (
     <ActionIcon
       aria-label="Theme switch"
