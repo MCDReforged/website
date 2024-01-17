7adc456e26a0f0554bb46f752770493c9a64da55
Fallen_Breath <nellaforax@outlook.com>
2024-01-17 23:57:57 +0800

    proper theme switch icon switch logic

--- src/components/ui/theme-switch.tsx
+++ src/components/ui/theme-switch.tsx
@@ -1,19 +1,10 @@
-'use client'
-
 import { ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
+import styles from "./theme-switch.module.css"
 
 export function ThemeSwitch({...props}) {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const { toggleColorScheme } = useMantineColorScheme()
 
-  // https://nextjs.org/docs/messages/react-hydration-error#solution-1-using-useeffect-to-run-on-the-client-only
-  const [isServer, setIsServer] = useState(true)
-  useEffect(() => {
-    setIsServer(false)
-  }, [])
-
-  const icon = isServer || colorScheme === 'light' ? <IconSun stroke={1.5} /> : <IconMoon stroke={1.5} />
   return (
     <ActionIcon
       aria-label="Theme switch"
@@ -22,7 +13,8 @@ export function ThemeSwitch({...props}) {
       onClick={toggleColorScheme}
       {...props}
     >
-      {icon}
+      <IconSun stroke={1.5} className={styles.sun}/>
+      <IconMoon stroke={1.5} className={styles.moon}/>
     </ActionIcon>
   )
 }
