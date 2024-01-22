06db16a1fc0ef3bce2b1839178d24bad157bb026
Fallen_Breath <nellaforax@outlook.com>
2024-01-22 17:36:26 +0800

    dynamically updating TimeAgo

--- src/components/time-ago.tsx
+++ src/components/time-ago.tsx
@@ -3,17 +3,40 @@
 import { formatTime, getTimeAgo } from "@/utils/time-utils";
 import { Tooltip } from "@mantine/core";
 import { useLocale } from "next-intl";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+
+interface TimeTexts {
+  timeAgo: string
+  timeFormatted: string
+}
+
+function createTimeTexts(date: Date, locale: string): TimeTexts {
+  return {
+    timeAgo: getTimeAgo(date, locale),
+    timeFormatted: formatTime(date, 'LLL', locale),
+  }
+}
 
 export function TimeAgo({date, className, component: Component = 'p'}: { date: Date, className?: string, component?: React.ElementType }) {
   const locale = useLocale()
+  const [timeTexts, setTimeTexts] = useState<TimeTexts>(createTimeTexts(date, locale))
 
-  const timeAgo = getTimeAgo(date, locale)
-  const timeFormatted = formatTime(date, 'LLL', locale)
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        const newTexts = createTimeTexts(date, locale)
+        if (newTexts.timeAgo !== timeTexts.timeAgo || newTexts.timeFormatted !== timeTexts.timeFormatted) {
+          setTimeTexts(newTexts)
+        }
+      },
+      60 * 1000
+    )
+    return () => clearInterval(interval)
+  }, [date, locale, timeTexts])
 
   return (
-    <Tooltip label={timeFormatted}>
-      <Component className={className}>{timeAgo}</Component>
+    <Tooltip label={timeTexts.timeFormatted}>
+      <Component className={className}>{timeTexts.timeAgo}</Component>
     </Tooltip>
   )
 }
--- src/utils/time-utils.ts
+++ src/utils/time-utils.ts
@@ -1,13 +1,14 @@
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 
-export function getTimeAgo(date?: Date, locale?: string): string | undefined {
+// NOTES: if SSR, the result might be wrong
+export function getTimeAgo(date: Date, locale?: string): string {
   moment.locale(locale?.toLowerCase())
-  return date === undefined ? undefined : moment(date).fromNow()  // TODO: check if ssr will be an issue
+  return moment(date).fromNow()
 }
 
-export function formatTime(date?: Date, formatter: string = 'YYYY/MM/DD hh:mm:ss', locale?: string): string | undefined {
+export function formatTime(date: Date, formatter: string = 'YYYY/MM/DD hh:mm:ss', locale?: string): string {
   moment.locale(locale?.toLowerCase())
-  return date === undefined ? undefined : moment(date).format(formatter)
+  return moment(date).format(formatter)
 }
 
