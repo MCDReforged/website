27ce242c980311c6425328d9ad236e8ac455edc0
Fallen_Breath <nellaforax@outlook.com>
2024-01-22 15:02:50 +0800

    enable nextjs fetch logging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
 const withNextIntl = require('next-intl/plugin')()
 
 /** @type {import('next').NextConfig} */
@@ -9,6 +8,11 @@ const nextConfig = withBundleAnalyzer(withNextIntl({
   experimental: {
     // https://mantine.dev/guides/next/#app-router-tree-shaking
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 }))
 
