c674c74170bd98ce06f50c8708ae76eec037768e
Fallen_Breath <nellaforax@outlook.com>
2024-03-03 19:17:28 +0800

    fixed /plugin page redirect render

--- src/app/[locale]/plugin/page.tsx
+++ src/app/[locale]/plugin/page.tsx
@@ -1,6 +1,9 @@
 import { redirect } from "@/common/navigation";
 import { routes } from "@/site/routes";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Page() {
+export default function Page({params: {locale}}: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale)
+
   redirect(routes.catalogue())
 }
